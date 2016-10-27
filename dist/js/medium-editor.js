964d25b54945220f83f0ae1cdaf9ac44064ede73
Max Brodin <maxb@pressreader.com>
2016-10-27 11:04:36 -0700

    Update dist files to fix firefox bug

--- dist/js/medium-editor.js
+++ dist/js/medium-editor.js
@@ -4305,7 +4305,8 @@ MediumEditor.extensions = {};
     var WHITESPACE_CHARS,
         KNOWN_TLDS_FRAGMENT,
         LINK_REGEXP_TEXT,
-        KNOWN_TLDS_REGEXP;
+        KNOWN_TLDS_REGEXP,
+        LINK_REGEXP;
 
     WHITESPACE_CHARS = [' ', '\t', '\n', '\r', '\u00A0', '\u2000', '\u2001', '\u2002', '\u2003',
                                     '\u2028', '\u2029'];
@@ -4326,6 +4327,8 @@ MediumEditor.extensions = {};
         ')|(([a-z0-9\\-]+\\.)?[a-z0-9\\-]+\\.(' + KNOWN_TLDS_FRAGMENT + '))';
 
     KNOWN_TLDS_REGEXP = new RegExp('^(' + KNOWN_TLDS_FRAGMENT + ')$', 'i');
+
+    LINK_REGEXP = new RegExp(LINK_REGEXP_TEXT, 'gi');
 
     function nodeIsNotInsideAnchorTag(node) {
         return !MediumEditor.util.getClosestTag(node, 'a');
@@ -4508,12 +4511,11 @@ MediumEditor.extensions = {};
         },
 
         findLinkableText: function (contenteditable) {
-            var linkRegExp = new RegExp(LINK_REGEXP_TEXT, 'gi'),
-                textContent = contenteditable.textContent,
+            var textContent = contenteditable.textContent,
                 match = null,
                 matches = [];
 
-            while ((match = linkRegExp.exec(textContent)) !== null) {
+            while ((match = LINK_REGEXP.exec(textContent)) !== null) {
                 var matchOk = true,
                     matchEnd = match.index + match[0].length;
                 // If the regexp detected something as a link that has text immediately preceding/following it, bail out.
@@ -5406,13 +5408,6 @@ MediumEditor.extensions = {};
             pasteBinElm.innerHTML = pasteBinDefaultContent;
 
             this.document.body.appendChild(pasteBinElm);
-
-            // avoid .focus() to stop other event (actually the paste event)
-            this.on(pasteBinElm, 'focus', stopProp);
-            this.on(pasteBinElm, 'focusin', stopProp);
-            this.on(pasteBinElm, 'focusout', stopProp);
-
-            pasteBinElm.focus();
 
             MediumEditor.selection.selectNode(pasteBinElm, this.document);
 
